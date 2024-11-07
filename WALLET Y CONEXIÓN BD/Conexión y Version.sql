c526277e5dbc3b91eb30f8e819f4de3ed8889308
DeytonGarcia <136906803+DeytonGarcia@users.noreply.github.com>
2024-11-07 11:33:12 -0500

    Update Conexión y Version.sql

--- WALLET Y CONEXIÓN BD/Conexión y Version.sql
+++ WALLET Y CONEXIÓN BD/Conexión y Version.sql
@@ -1,1 +1,21 @@
+-- Para ver la versión q estamos utilizando
+SELECT * FROM V$VERSION;
 
+-- Ver usuarios de la instancia de base de datos
+SELECT username FROM DBA_USERS;
+
+-- Creación de Uusario Restaurante De Graciella
+CREATE USER Rgraciella IDENTIFIED BY DeytonPaolo0204A;
+
+-- Usuarios
+SELECT * FROM DBA_USERS;
+
+-- Privilegios 
+GRANT CREATE SESSION TO Rgraciella;
+GRANT CREATE TABLE TO Rgraciella;
+GRANT CREATE VIEW TO Rgraciella;
+GRANT CREATE PROCEDURE TO Rgraciella;
+GRANT CREATE TRIGGER TO Rgraciella;
+GRANT CREATE SEQUENCE TO Rgraciella;
+GRANT CREATE SYNONYM TO Rgraciella;
+GRANT UNLIMITED TABLESPACE TO Rgraciella;

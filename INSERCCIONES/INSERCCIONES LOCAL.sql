818c731becd37766a52c9c51dd0cc8d6a2817986
DeytonGarcia <136906803+DeytonGarcia@users.noreply.github.com>
2024-11-07 11:15:35 -0500

    Add files via upload

---
+++ INSERCCIONES/INSERCCIONES LOCAL.sql
@@ -0,0 +1,19 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# SQL Server Management Studio Solution File, Format Version 19.00
+VisualStudioVersion = 15.0.28307.421
+MinimumVisualStudioVersion = 10.0.40219.1
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Default|Default = Default|Default
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{840AB76A-86ED-420F-B81F-454B2BB1A681}.Default|Default.ActiveCfg = Default
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {68949CA4-69C1-4264-93C4-2A6ADDC661E1}
+	EndGlobalSection
+EndGlobal
